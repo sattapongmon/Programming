e5317bb8964a531f20a02cb7736e8d25becdcf44
sattapongmon <52350359+sattapongmon@users.noreply.github.com>
2019-07-28 23:33:31 +0700

    HOMEWORK 2 Exercise4_sattapong_k

---
+++ Exercise4_sattapong_n.py
@@ -0,0 +1,9 @@
+F=10
+G=20
+I=30
+C=40
+print("-------Your Score-------")
+print("Foundation English",":",F)
+print("General Business",":",G)
+print("Introduction to Computer",":",I)
+print("Computer Programming",":",C)
