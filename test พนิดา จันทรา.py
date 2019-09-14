846f06a644f6e2f8fba8d9425332fcb5571c0d71
sattapongmon <52350359+sattapongmon@users.noreply.github.com>
2019-09-14 19:44:11 +0700

    Add files via upload

---
+++ test พนิดา จันทรา.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 2.แสดงราคาสินค้า
+# 
+
+# In[1]:
+
+
+A=input("---------Hello-------------")
+B=input("กล้วย  30 THB")
+C=input("น้ำเปล่า   20 THB") 
+D=input("แปรงสีฟัน   15  THB")
+total=input(B,"+",C,"+",D",THB")  
+print(Total)
+print("-----------good bye!!!---------")
+
+
+# # 4.แสดงผล helloและword
+
+# In[ ]:
+
+
+A=input("Hello")
+B=input("World")
+while true:
+    A
+    
+        
+    
+    
+
+
+# In[ ]:
+
+
+
+
